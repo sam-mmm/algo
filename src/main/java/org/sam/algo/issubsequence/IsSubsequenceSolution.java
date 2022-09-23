8807f8bd979ac0a21bb6bb0aae0d5b430a1ff4ff
sam-mmm <2740878+sam-mmm@users.noreply.github.com>
2022-09-23 16:28:43 +0530

    is sub sequence int arrays

--- src/main/java/org/sam/algo/issubsequence/IsSubsequenceSolution.java
+++ src/main/java/org/sam/algo/issubsequence/IsSubsequenceSolution.java
@@ -1,7 +1,34 @@
 package org.sam.algo.issubsequence;
 
+import java.util.List;
+
 public class IsSubsequenceSolution {
  public boolean isSubsequence(String s, String t) {
+     
         return false;
     }   
+ 
+ public boolean isSubsequenceIntArray(List<Integer> array, List<Integer> sequence) {
+     int arrI = 0;
+     int seqI = 0;
+     while(arrI<array.size() && seqI <sequence.size()){
+        if(array.get(arrI) == sequence.get(seqI)){
+            seqI++;
+        }
+        arrI++;
+    }   
+     return seqI == sequence.size();
+ }
+ 
+ public boolean isSubsequenceIntArray2(List<Integer> array, List<Integer> sequence) {
+     int seqI = 0;
+     for (Integer i : array) {
+         if(seqI == sequence.size()) return true;
+         if(sequence.get(seqI)== i){
+             seqI++;
+         }
+     }
+   
+     return seqI == sequence.size();
+ }
 }
---
+++ src/test/java/org/sam/algo/issubsequence/IsSubsequenceSolutionTest.java
@@ -0,0 +1,54 @@
+package org.sam.algo.issubsequence;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class IsSubsequenceSolutionTest {
+
+    IsSubsequenceSolution solution;
+
+    @BeforeClass
+    public void setUp() {
+        solution = new IsSubsequenceSolution();
+    }
+
+     @DataProvider(name = "test1")
+    public Object[][] createData1() {
+        return new Object[][]{
+            new Object[]{"ahbgdc", "abc", true},
+            new Object[]{"ahbgdc", "axc", false},
+        };
+    }
+    
+    @Test(dataProvider = "test1")
+    public void isSubsequence(String t,String s, boolean expected) {
+        boolean val = solution.isSubsequence(t,s);
+        assertEquals(val, expected);
+    }
+    
+    @DataProvider(name = "test2")
+    public Object[][] createData2() {
+        return new Object[][]{
+            new Object[]{new Integer[]{5, 1, 22, 25, 6, -1, 8, 10}, new Integer[]{1, 6, -1, 10}, true},
+            new Object[]{new Integer[]{5, 1, 22, 25, 6, -1, 8, 10, 4, 21}, new Integer[]{1, 6, -1, 10}, true},
+            new Object[]{new Integer[]{5, 1, 22, 25, 6, -1, 8, 4, 21}, new Integer[]{1, 6, -1, 10}, false},
+        };
+    }
+
+    @Test(dataProvider = "test2")
+    public void isSubsequenceIntArray(Integer[] arr, Integer[] seq, boolean expected) {
+        boolean val = solution.isSubsequenceIntArray(Arrays.asList(arr), Arrays.asList(seq));
+        assertEquals(val, expected);
+    }
+    
+    @Test(dataProvider = "test2")
+    public void isSubsequenceIntArray2(Integer[] arr, Integer[] seq, boolean expected) {
+        boolean val = solution.isSubsequenceIntArray2(Arrays.asList(arr), Arrays.asList(seq));
+        assertEquals(val, expected);
+    }
+}
